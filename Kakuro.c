dce7610d60f8da8fbf70c3f9019d011017518cfa
Adryangab <adryanngab@gmail.com>
2025-09-16 08:39:55 -0300

    Crei função de Ler tamanho da matriz

--- Kakuro.c
+++ Kakuro.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+int ler_tamanho_matriz(){
+    int numero;
+    do{
+    printf("Escolha o tamanho da matriz do Kakuro (Max. 8)");
+    scanf("%d", &numero);}while(numero <1 || numero > 8);
+    return numero;
+}
+
 void main()
 {
-    
+    int tamanho;
+    tamanho = ler_tamanho_matriz();   
 }
