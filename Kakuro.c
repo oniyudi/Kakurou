7275d9b370ab9eb5ec5dcda58b3c78db9fd8cb0b
oniyudi <onimaru.yudi@gmail.com>
2025-09-23 08:39:57 -0300

    preenchendo a matriz do jogo

--- Kakuro.c
+++ Kakuro.c
@@ -10,6 +10,21 @@ int ler_tamanho_matriz(){
     return numero;
 }
 
+void efetuar_soma_colunas(int mat[][8], int tam)
+{
+    int i, c, soma = 0;
+    mat[0][0] = 0;
+    for(c = 1; c < tam; c++)
+    {
+        for(i = 1; i < tam; i++)
+        {
+            soma += mat[i][c];
+        }
+        mat[0][c] = soma;
+        soma = 0;
+    }
+}
+
 void preencher_vetor(int mat[][8],int tam){
     int i,c,excluido,soma = 0, sorteio;
 
@@ -18,7 +33,6 @@ void preencher_vetor(int mat[][8],int tam){
             excluido = rand() %2; //excluido, é para saber se a posicao vai ter um valor ou não;
             if(excluido == 1){
                 sorteio = rand() %9+1; //preenchendo a posicao com um valor aleatorio;
-                printf("Sorteio = %d; linha = %d; Coluna = %d\n", sorteio, i, c);
                 mat[i][c] = sorteio;
                 soma+=sorteio;
             }
@@ -30,11 +44,8 @@ void preencher_vetor(int mat[][8],int tam){
         mat[i][0] = soma;
         soma = 0;
     }
-}
-
-void efetuar_soma_colunas(int mat[][8], int tam)
-{
-    int i, c, soma = 0;
+    
+    efetuar_soma_colunas(mat, tam);
 }
 
 void apresentar_matriz(int mat[][8], int tam)
@@ -50,10 +61,31 @@ void apresentar_matriz(int mat[][8], int tam)
     }
 }
 
+void preencher_matriz_jogo(int mat_main[][8], int mat_game[][8], int tam)
+{
+    int i, c, sorteio;
+    for(i = 0; i < tam; i++)
+    {
+        for(c = 0; c < tam; c++)
+        {
+            if(mat_main[i][c] == 0)
+            {
+                sorteio = rand() %9+1;
+                mat_game[i][c] = sorteio;
+            }
+            else
+            {
+                mat_game[i][c] = mat_main[i][c];
+            }
+        }
+    }
+    mat_game[0][0] = 0;
+}
+
 int main()
 {
     srand(time(NULL));
-    int tamanho, matriz_principal[8][8] /*matriz_b[8][8]*/; // matriz principal é a colinha
+    int tamanho, matriz_principal[8][8], matriz_b[8][8]; // matriz principal é a colinha
     tamanho = ler_tamanho_matriz();
 
     printf("O tamanho da matriz: %d\n", tamanho);
@@ -61,6 +93,10 @@ int main()
     preencher_vetor(matriz_principal, tamanho);
 
     apresentar_matriz(matriz_principal, tamanho);
+
+    preencher_matriz_jogo(matriz_principal, matriz_b, tamanho);
+    printf("\nApresentando a matriz do jogador:\n");
+    apresentar_matriz(matriz_b, tamanho);
 
     return 0;
 }
